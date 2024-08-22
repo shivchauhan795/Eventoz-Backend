26a3bafcac249bff1f0395401486f6943c312910
Shiv Chauhan <shivchauhan795@gmail.com>
2024-08-22 17:45:19 +0530

    added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config()
 const mongourl = process.env.MONGO_URL
 const client = new MongoClient(mongourl, {
     tls: true,  // Enable TLS
-    tlsInsecure: false,  // Ensure certificates are validated
+    tlsInsecure: true,  // Ensure certificates are validated
     connectTimeoutMS: 10000,
 })
 const dbName = 'eventoz'
